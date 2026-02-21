fa5c271099982f8b30c6dc1295fa3b511ea5b5f6
Jose Carlos Cardenas Martinez <josenanodev@gmail.com>
2026-02-21 09:04:33 -0600

    feat: add image deletion functionality in admin panel and update image handling logic

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import awsAmplify from "astro-aws-amplify";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://josenanodev.de",
   output: "server",
   adapter: awsAmplify(),
   image: {
